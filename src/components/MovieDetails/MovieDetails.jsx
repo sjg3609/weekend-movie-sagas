8a48d561cdb9fcc973b82819203519fcd1cbdc35
Sam Gossie <sjg3609@gmail.com>
2023-05-04 08:59:31 -0500

    Working on getting the movie descriptsion to map to the Movie Details page

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,11 +1,21 @@
-
+import { useSelector } from 'react-redux';
 
 
 function MovieDetails() {
 
+    const movies = useSelector(store => store.movies);
+
     return (
         <>
             <h3>Movie Details</h3>
+            {
+                movies.map(movie => {
+                    <div className="movieDetails" key={movie.id}>
+                         <h4>{movies.description}</h4>
+                    </div>
+                    
+                })
+            }
         </>
     )
 }
